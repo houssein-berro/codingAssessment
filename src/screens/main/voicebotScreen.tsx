f7e3b6d1aa1b9f3559368a3213adcba848137440
Berro-1 <hussienberro1@gmail.com>
2025-01-29 14:31:09 +0200

    Added empty screens for main navigator

---
+++ src/screens/main/voicebotScreen.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const VoicebotScreen = () => {
+  return (
+    <View>
+      <Text>VoicebotScreen</Text>
+    </View>
+  )
+}
+
+export default VoicebotScreen
+
+const styles = StyleSheet.create({})
