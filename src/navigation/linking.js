fb9b860f5ad1a065d7c79bb124c1dc34c6fa2d81
Berro-1 <hussienberro1@gmail.com>
2025-02-03 18:02:09 +0200

    Updated linking.js file to open the main screen before opening the setCompanyId screen

--- src/navigation/linking.js
+++ src/navigation/linking.js
@@ -4,21 +4,9 @@ export const linking = {
   prefixes: ['myapp://myapp.com', 'https://myapp.com'],
   config: {
     screens: {
-      Main: {
-        screens: {
-          SettingsStack: {
-            screens: {
-              SetCompanyID: 'set-company-id',
-            },
-          },
-        },
-      },
+      MainScreen: 'MainScreen',
+      Voicebot: 'voicebot',
     },
   },
-  getStateFromPath: (path, options) => {
-    console.log('Parsing path:', path);
-    const state = defaultGetStateFromPath(path, options);
-    console.log('Resulting navigation state:', JSON.stringify(state, null, 2));
-    return state;
-  },
+  getStateFromPath: (path, options) => defaultGetStateFromPath(path, options),
 };
